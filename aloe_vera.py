db5a9309de863f93ab13db17a3bcfda94730c337
Jack Gourdeau <67436233+JGourdeau@users.noreply.github.com>
2022-01-08 22:10:23 -0500

    characterize sliders

--- aloe_vera.py
+++ aloe_vera.py
@@ -1,53 +1,86 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import StringVar, ttk
+from typing_extensions import IntVar
 
+
+BACKGROUND = '#4b8d2f'
 
 # define the aloe_vera window
 def AVeraWindow(root): 
     root.title('Aloe Vera')
-    root.geometry('525x500')
-    root.configure(bg='#4b8d2f')
+    root.geometry('800x300')
+    root.configure(bg=BACKGROUND)
     root.grid()
+    
+    global lev, lev_color
+    lev = StringVar(root, value='Resting State')
+    lev_color = StringVar(root, value='White')
+    
     createWidgets(root)
+   
     return root
 
 # create a slider
-def createSlider(container, f, t): 
-    s = tk.Scale(container, bg='#4b8d2f', orient='horizontal', fg='white', activebackground='blue', 
+def createSlider(container, f, t, c=None, v=None): 
+    s = tk.Scale(container, bg=BACKGROUND, orient='horizontal', fg='white', activebackground='blue', 
                  from_=int(f), to=int(t), 
-                 length=300)
+                 length=300, command=c, variable=v)
     return s
 
+# create a label
 def createLabel(container, t):
-    l = tk.Label(text=t, background='#4b8d2f', foreground='white', justify='left', anchor=tk.W, padx=25, pady=25)
+    l = tk.Label(container, text=t, background=BACKGROUND, foreground='white', justify='left', anchor=tk.W, padx=25, pady=25)
     return l
 
 
+def update_stress_var(var):
+    global lev, lev_color
+    new_val = int(var)
+    
+    if new_val < 26: 
+        lev.set('Resting State')
+
+    elif new_val < 51: 
+        lev.set('Low Stress')
+
+    elif new_val < 76:
+        lev.set('Medium Stress')
+
+    else: 
+        lev.set('High Stress')
+    
+    
+    
 def createWidgets(container):
+    global lev, lev_color
 
-    # create the stress label: 
-    stress_label = createLabel(container, 'What was your percieved stress level today?:\n0–25: Resting state, \n26–50: Low stress, \n51–75: Medium stress,\n76–100: High stress')
+    # create the stress label:
+    stress_label = createLabel(container, 'What was your percieved stress level today?')
     stress_label.grid(row=0, column=0)
-
+    
     # create and grid the stress slider
-    stress_slider = createSlider(container, 0, 100)
+    stress_slider = createSlider(container, 0, 100, c=update_stress_var)
     stress_slider.grid(row=0, column=1)
+    
+    # categorize the slider state to the right
+    level_label = tk.Label(container, background=BACKGROUND, textvariable=lev, padx=25, pady=25, fg='white')
+    level_label.grid(row=0, column = 3)
 
     # create the energy label: 
-    energy_label = createLabel(container, 'This is a energy slider: ')
-    energy_label.grid(row=1, column=0)
+    sleep_label = createLabel(container, 'How well do you think you slept last night?')
+    sleep_label.grid(row=1, column=0)
 
     # create and grid the energy slider
-    energy_slider = createSlider(container, 0, 100)
-    energy_slider.grid(row=1, column=1)
+    sleep_slider = createSlider(container, 0, 100)
+    sleep_slider.grid(row=1, column=1)
 
     # create the energy label: 
-    rank_label = createLabel(container, 'This is a energy slider: ')
-    rank_label.grid(row=2, column=0)
+    body_battery_label = createLabel(container, 'Where do you think your body battery is right now?')
+    body_battery_label.grid(row=2, column=0)
 
     # create and grid the energy slider
-    rank_slider = createSlider(container, 0, 10)
-    rank_slider.grid(row=2, column=1)
+    body_batter_slider = createSlider(container, 0, 100)
+    body_batter_slider.grid(row=2, column=1)
 
     
 def main(): 
