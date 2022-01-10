c43c07583e33803e67f81a23bc3973682dd858c0
Jack Gourdeau <67436233+JGourdeau@users.noreply.github.com>
2022-01-10 00:39:07 -0500

    submit button

--- aloe_vera.py
+++ aloe_vera.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import StringVar, ttk
 from typing_extensions import IntVar
 
-
 BACKGROUND = '#4b8d2f'
 
 # define the aloe_vera window
@@ -12,9 +11,13 @@ def AVeraWindow(root):
     root.configure(bg=BACKGROUND)
     root.grid()
     
-    global lev, lev_color
-    lev = StringVar(root, value='Resting State')
-    lev_color = StringVar(root, value='White')
+    global stress_lev, sleep_lev, body_bat_lev, stress_int, sleep_int, body_bat_int
+    stress_lev = StringVar(root, value='Resting State')
+    sleep_lev = StringVar(root, value='Poor')
+    body_bat_lev = StringVar(root, value='0')
+    stress_int = tk.IntVar(root, value=0)
+    sleep_int = tk.IntVar(root, value=0)
+    body_bat_int = tk.IntVar(root, value=0)
     
     createWidgets(root)
    
@@ -32,55 +35,88 @@ def createLabel(container, t):
     l = tk.Label(container, text=t, background=BACKGROUND, foreground='white', justify='left', anchor=tk.W, padx=25, pady=25)
     return l
 
-
-def update_stress_var(var):
-    global lev, lev_color
+# updates the sleep level label
+def update_sleep_lev(var):
+    global sleep_lev
+    new_val = int(var)
+    
+    if new_val > 99: 
+        sleep_lev.set('Excellent')
+    elif new_val > 79: 
+        sleep_lev.set('Good')
+    elif new_val > 49:
+        sleep_lev.set('Moderate')
+    else: 
+        sleep_lev.set('Poor')
+    
+# updates the stress level label
+def update_stress_lev(var):
+    global stress_lev
     new_val = int(var)
     
     if new_val < 26: 
-        lev.set('Resting State')
-
+        stress_lev.set('Resting State')
     elif new_val < 51: 
-        lev.set('Low Stress')
-
+        stress_lev.set('Low Stress')
     elif new_val < 76:
-        lev.set('Medium Stress')
-
+        stress_lev.set('Medium Stress')
     else: 
-        lev.set('High Stress')
+        stress_lev.set('High Stress')
+
+# update the body battery label
+def update_battery_level(var):
+    global body_bat_lev
+    body_bat_lev.set(var)
+
+def submit_vals():
+    global stress_int, sleep_int, body_bat_int
+    print(stress_int.get(), sleep_int.get(), body_bat_int.get())
     
     
-    
+
+# creates the widgets to go in the container passed to it 
 def createWidgets(container):
-    global lev, lev_color
+    global stress_lev, sleep_lev, stress_int, body_bat_int, sleep_int
 
     # create the stress label:
     stress_label = createLabel(container, 'What was your percieved stress level today?')
     stress_label.grid(row=0, column=0)
     
     # create and grid the stress slider
-    stress_slider = createSlider(container, 0, 100, c=update_stress_var)
+    stress_slider = createSlider(container, 0, 100, c=update_stress_lev, v=stress_int)
     stress_slider.grid(row=0, column=1)
     
     # categorize the slider state to the right
-    level_label = tk.Label(container, background=BACKGROUND, textvariable=lev, padx=25, pady=25, fg='white')
+    level_label = tk.Label(container, background=BACKGROUND, textvariable=stress_lev, padx=25, pady=25, fg='white')
     level_label.grid(row=0, column = 3)
 
-    # create the energy label: 
+    # create the sleep label: 
     sleep_label = createLabel(container, 'How well do you think you slept last night?')
     sleep_label.grid(row=1, column=0)
 
-    # create and grid the energy slider
-    sleep_slider = createSlider(container, 0, 100)
+    # create and grid the sleep slider
+    sleep_slider = createSlider(container, 0, 100, c=update_sleep_lev, v=sleep_int)
     sleep_slider.grid(row=1, column=1)
 
+    # categorize the slider state to the right
+    sleep_level_label = tk.Label(container, background=BACKGROUND, textvariable=sleep_lev, padx=25, pady=25, fg='white')
+    sleep_level_label.grid(row=1, column = 3)
+
     # create the energy label: 
-    body_battery_label = createLabel(container, 'Where do you think your body battery is right now?')
-    body_battery_label.grid(row=2, column=0)
+    body_battery_q = createLabel(container, 'Where do you think your body battery is right now?')
+    body_battery_q.grid(row=2, column=0)
 
     # create and grid the energy slider
-    body_batter_slider = createSlider(container, 0, 100)
-    body_batter_slider.grid(row=2, column=1)
+    body_battery_slider = createSlider(container, 0, 100, c=update_battery_level, v=body_bat_int)
+    body_battery_slider.grid(row=2, column=1)
+
+    # categorize the slider state to the right
+    body_battery_label = tk.Label(container, background=BACKGROUND, textvariable=body_bat_lev, padx=25, pady=25, fg='white')
+    body_battery_label.grid(row=2, column = 3)
+
+    # 
+    submit_button = tk.Button(container, text='Submit', highlightbackground=BACKGROUND, fg='white', activeforeground='blue', command=submit_vals)
+    submit_button.grid(row=3, column=1)
 
     
 def main(): 
